6efd20e0a43f738441b2f39f6248e8e44cf611a4
sneha476 <athotasneha474@gmail.com>
2026-06-17 20:08:25 +0530

    added problem sloving folder

---
+++ OneDrive/Desktop/problem solving/singlenum.py
@@ -0,0 +1,12 @@
+l=[1,2,2,3,1,1]
+for i in range(len(l)):
+    if(l.count(i)==1):
+        print(i)
+
+# for i in range(len(l)):
+#     count=0
+#     for j in range(len(l)):
+#         if(l[i]==l[j]):
+#             count+=1
+#     if(count==1):
+#         print(i)
